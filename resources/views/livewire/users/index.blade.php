0ad24390bc30f45cc1d09a351242d65f466463af
MariosTheof <athmarios@gmail.com>
2024-04-15 12:07:15 +0300

    Commented edit buttons

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -15,7 +15,7 @@
                 <td>{{ $user->email }}</td>
 
                 <td>
-                    <x-secondary-button wire:click="edit({{ $user->id }})">Edit</x-secondary-button>
+{{--                    <x-secondary-button wire:click="edit({{ $user->id }})">Edit</x-secondary-button>--}}
                     <x-secondary-button wire:click="delete({{ $user->id }})">Delete</x-secondary-button>
                 </td>
             </tr>
